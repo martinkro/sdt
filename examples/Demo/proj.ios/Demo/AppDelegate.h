beedb90d877e47bf1697e293fc224a85b7512c80
sundayliu <martinkro@aliyun.com>
2014-12-26 17:50:18 +0800

    Demo for ios

---
+++ examples/Demo/proj.ios/Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Demo
+//
+//  Created by gavingu on 14/12/26.
+//  Copyright (c) 2014年 sundayliu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ examples/Demo/proj.ios/Demo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Demo
+//
+//  Created by gavingu on 14/12/26.
+//  Copyright (c) 2014年 sundayliu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
